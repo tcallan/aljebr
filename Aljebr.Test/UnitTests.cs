6382697e31788cf82fb7efa2239d86c6d29d5d81
tcallan <terry.callan.iii@gmail.com>
2016-09-01 13:18:57 -0400

    Add Unit type and Lift method

---
+++ Aljebr.Test/UnitTests.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Aljebr.Test
+{
+   [TestClass]
+   public class UnitTests
+   {
+      [TestMethod]
+      public void TestLiftDoesStuffWhenCalled1()
+      {
+         var called = false;
+         Action<bool> action = param => called = param;
+
+         Unit.Lift(action)(true);
+
+         Assert.IsTrue(called);
+      }
+
+      [TestMethod]
+      public void TestLiftDoesStuffWhenCalled2()
+      {
+         var result = string.Empty;
+         Action<string> action = param => result = param;
+
+         Unit.Lift(action)("foo");
+
+         Assert.AreEqual("foo", result);
+      }
+
+      [TestMethod]
+      public void TestLiftDoesNotDoStuffWhenNotCalled()
+      {
+         var called = false;
+         Action<bool> action = param => called = true;
+
+         Unit.Lift(action);
+
+         Assert.IsFalse(called);
+      }
+
+      [TestMethod]
+      public void TestLiftReturnsUnit()
+      {
+         Action<string> action = Console.WriteLine;
+
+         var result = Unit.Lift(action)("foo");
+
+         Assert.AreEqual(Unit.Value, result);
+      }
+
+      [TestMethod]
+      public void TestLiftWorksAsExpectedWithMatching1()
+      {
+         var union = new Union<char, int>('c');
+         var result = TestResult.UnexpectedResult;
+
+         union.With<Unit>()
+            .Match(Unit.Lift<char>(c => result = c.CharAsExpected()))
+            .Match(Unit.Lift<int>(i => result = i.IntAsUnexpected()))
+            .Do();
+
+         result.AssertExpected();
+      }
+
+      [TestMethod]
+      public void TestLiftWorksAsExpectedWithMatching2()
+      {
+         var union = new Union<char, int>(12);
+         var result = TestResult.UnexpectedResult;
+
+         union.With<Unit>()
+            .Match(Unit.Lift<char>(c => result = c.CharAsUnexpected()))
+            .Match(Unit.Lift<int>(i => result = i.IntAsExpected()))
+            .Do();
+
+         result.AssertExpected();
+      }
+   }
+}
---
+++ Aljebr/Unit.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aljebr
+{
+   public struct Unit
+   {
+      public static Unit Value;
+
+      public static Func<T, Unit> Lift<T>(Action<T> action)
+      {
+         return arg =>
+         {
+            action(arg);
+            return Value;
+         };
+      }
+   }
+}
